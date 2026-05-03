3c6060ee636fdef0f66dad0c0a586bb952dd2546
benja0952346729-hash <benja0952346729@gmail.com>
2026-05-03 09:50:51 +0300

    Update bingo_bot.py

--- bingo_bot.py
+++ bingo_bot.py
@@ -25,8 +25,7 @@ from flask import Flask
 # ══════════════════════════════════════════════
 BOT_TOKEN  = os.environ.get("BOT_TOKEN", "")
 ADMIN_ID   = 6883208728
-WEBAPP_URL = "https://bingo-game-4.onrender.com"
-
+WEBAPP_URL = "https://game-production-7f86.up.railway.app"
 FIREBASE_DB_URL = "https://house-rent-app-3674a-default-rtdb.firebaseio.com/"
 
 MIN_WITHDRAWAL = 50
